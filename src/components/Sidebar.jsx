7007884eca993565ae470fb9ef5e7f705a2f13ec
Fab1oGu1 <fabio2011141@gmail.com>
2023-03-04 16:07:13 -0300

    finalizado

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,22 @@
+import SocialNetworkContainer from "./SocialNetworkContainer";
+import InformationContainer from "./InformationContainer";
+
+import Avatar from "../img/eu.jpg";
+
+import "../styles/components/sidebar.sass";
+
+const Sidebar = () => {
+  return (
+    <aside id="sidebar">
+      <img src={Avatar} alt="Fábio Guimarães" />
+      <p className="title">Desenvolvedor</p>
+      <SocialNetworkContainer />
+      <InformationContainer />
+      <a href="#" className="btn">
+        Download currículo
+      </a>
+    </aside>
+  );
+};
+
+export default Sidebar;
